b2ed01ce34320831cdca6decb4a34614fbeb78ef
SJM <kraev.maksim2003@gmail.com>
2025-02-13 09:50:16 +0700

    добавил систему клика на клетку

---
+++ Assets/Scripts/Data/CellInitData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CellInitData", menuName = "Scriptable Objects/CellInitData")]
+public class CellInitData : ScriptableObject
+{
+    public GameObject CellPrefab;
+}
